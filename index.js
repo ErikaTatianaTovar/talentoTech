2f6f4a2a94254a8fa78f3cc0f1d4402632d31bc5
Erika Tovar <tatianatovar59@gmail.com>
2024-02-17 11:57:58 -0500

    new index

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express') //importar libreria
-const app = express() //inicializar variable que usa la libreria
-const router = express.Router() //enrutar servicios web
-const port = 3000 //escuchar la ejecucion del sewrvidor
-const UserSchema = require('./models/User.js')
+const express = require('express') //Importo la libreria
+const app = express() //Inicializacion de la variable que usara la libreria
+const router = express.Router(); // Enrutar los servicios web
+const port = 3000; // Escuchar la ejecucion del servidor
+require('dotenv').config()
 
-const mongoose = require('mongoose') //importar libreria
-mongoose.connect("mongodb+srv://ErikaTovar:tati25tovar@cluster0.mjo7nb8.mongodb.net/talentotech")
+const DB_URL = process.env.DB_URL || '';
 
-const userRoutes = require('./routes/UserRoutes')
+const mongoose = require('mongoose'); // Importo la libreria mongoose
+mongoose.connect(DB_URL) // Creo la cadena de conexion
 
+const userRoutes = require('./routes/UserRoutes');
 
-app.use(express.urlencoded({extended:true})) //ACCEDER A LAS INFOS DE LA URLS
-app.use(express.json()) //Analizar la info en json
+app.use(express.urlencoded({extended: true})) // Acceder a la informacion de las urls
+app.use(express.json()) // Analizar informacion en formato JSON
+
+//Metodo [GET, POST, PUT, PATCH, DELETE]
+// Nombre del servicio [/]
+router.get('/', (req, res) => {
+    //Informacion a modificar
+    res.send("Hello world")
+})
 
 //Ejecuto el servidor
 app.use(router)
