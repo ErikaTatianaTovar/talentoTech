e4962ac31ff64d75e86a31d46e6d8eef7cdd2a36
Erika Tovar <tatianatovar59@gmail.com>
2024-02-21 18:16:42 -0500

    .

---
+++ models/House.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose') // Importando la libreria
+
+// Creando el modelo de house
+const HouseSchema = new mongoose.Schema({
+
+})
+
+module.exports = mongoose.model('house', HouseSchema) 
